d45f98ab29c062bd90fb709021593ae214560181
Jan Pokorný <jan.pokorny@vacuumlabs.com>
2022-10-12 13:36:12 +0200

    Fix issue parsing in jira-checkout completions

--- dot_config/fish/completions/jira-checkout.fish
+++ dot_config/fish/completions/jira-checkout.fish
@@ -1,1 +1,1 @@
-complete --exclusive --command jira-checkout --arguments '$(jira issue list -a$(jira me) --plain | tail -n +2 | awk -F\'\t\' -v OFS=\'\t\' \'{ print $2, $3 }\')'
+complete --exclusive --command jira-checkout --arguments '$(jira issue list -a$(jira me) --plain | tail -n +2 | string replace --all --regex \'\\t+\' \'\t\' | awk -F\'\t\' -v OFS=\'\t\' \'{ print $2, $3 }\')'
