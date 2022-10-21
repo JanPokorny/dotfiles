5de49b1a279ef7b086a3967782069c440594e6ef
Jan Pokorný <jan.pokorny@vacuumlabs.com>
2022-10-21 17:17:10 +0200

    Add common aliases

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,3 +2,11 @@ status is-interactive || return
 
 # No greeting
 set fish_greeting
+
+# Tide config
+set -gx tide_git_truncation_length 50
+
+# Common aliases
+alias cat 'bat'
+alias ls 'exa --oneline --icons --group-directories-first'
+alias clip 'xsel --clipboard'
