637987a8c2af205106c9cc2ec6f0acc25c9a700f
Jan Pokorný <jan.pokorny@vacuumlabs.com>
2022-10-25 15:57:26 +0200

    Limit suggestions for jira-checkout to relevant ones

--- dot_config/fish/completions/jira-checkout.fish
+++ dot_config/fish/completions/jira-checkout.fish
@@ -1,1 +1,1 @@
-complete --exclusive --command jira-checkout --arguments '$(jira issue list -a$(jira me) --plain | tail -n +2 | string replace --all --regex \'\\t+\' \'\t\' | awk -F\'\t\' -v OFS=\'\t\' \'{ print $2, $3 }\')'
+complete --exclusive --command jira-checkout --arguments '$(jira issue list -a$(jira me) --plain | tail -n +2 | string replace --all --regex \'\\t+\' \'\t\' | awk -F\'\t\' -v OFS=\'\t\' \'{ if($4 == "To Do" || $4 == "In Progress" || $4 == "In Tech Review") print $2, $3 }\')'
