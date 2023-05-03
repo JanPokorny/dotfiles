bb97141e41d1a8964660d47ecb9c3a2915d443fe
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-03 14:50:15 +0200

    Update run_once_after_40-fonts.sh

--- .chezmoiscripts/run_once_after_40-fonts.sh
+++ .chezmoiscripts/run_once_after_40-fonts.sh
@@ -8,6 +8,7 @@ cd ~/.local/share/fonts
 
 echo "# JetBrains Mono NF"
 wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFont-Medium.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFontMono-Medium.ttf
 
 echo "# Refreshing font cache..."
 fc-cache -v -f .
