52d0658f9f78bdd9c38590da0e4d85927f51764f
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-03 17:26:58 +0200

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -18,9 +18,6 @@ fundle init
 # Configure fzf
 set -gx FZF_DEFAULT_COMMAND 'find . -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
 
-# Load starship
-starship init fish | source
-
 # Define common aliases
 alias cat 'bat --plain'
 alias ls 'exa --oneline --icons --group-directories-first'
