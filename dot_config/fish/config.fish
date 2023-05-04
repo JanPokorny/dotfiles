25e3f3a18ee9cb5f5b73d479a37f5d89ea5de356
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-04 17:28:07 +0200

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,7 +4,9 @@ status is-interactive || exit 0
 set fish_greeting
 
 # Configure fzf
-set -gx FZF_DEFAULT_COMMAND 'find . -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
+set -gx FZF_DEFAULT_COMMAND 'find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
+set -gx FZF_CTRL_T_COMMAND  'find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
+set -gx FZF_ALT_C_COMMAND   'find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -type d -print'
 
 # Define common aliases
 alias cat 'bat --plain --pager=never'
