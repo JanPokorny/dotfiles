5a838e6492bfa2b3b17f0faa1a478e4f4064edfc
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-04 18:15:41 +0200

    Update and rename run_once_after_40-fonts.sh to run_once_after_10-fonts.sh

--- .chezmoiscripts/run_once_after_10-fonts.sh
+++ .chezmoiscripts/run_once_after_10-fonts.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 echo ""
-echo "### FONTS ###"
+echo "### INSTALL FONTS ###"
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
-echo "# JetBrains Mono NF"
 wget -O https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFont-Medium.ttf
 wget -O https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFontMono-Medium.ttf
 
-echo "# Refreshing font cache..."
 fc-cache -v -f .
