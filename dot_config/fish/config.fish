270bc16550808e50c388c1fa1ea4e819f13d7134
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-04 11:05:11 +0200

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,18 +3,6 @@ status is-interactive || return
 # Disable greeting
 set fish_greeting
 
-# Load fundle plugins
-fundle plugin edc/bass
-fundle plugin gazorby/fish-abbreviation-tips
-fundle plugin nickeb96/puffer-fish
-fundle plugin jorgebucaran/autopair.fish
-fundle plugin oh-my-fish/plugin-thefuck
-fundle plugin oh-my-fish/plugin-direnv
-fundle plugin decors/fish-colored-man
-fundle plugin kidonng/zoxide.fish
-fundle plugin lig/fish-gitmoji --url https://codeberg.org/lig/fish-gitmoji.git
-fundle init
-
 # Configure fzf
 set -gx FZF_DEFAULT_COMMAND 'find . -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
 
