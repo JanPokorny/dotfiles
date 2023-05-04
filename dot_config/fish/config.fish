19ec377908dce2a1e1876f5a5c31c846d916a38d
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-04 14:31:53 +0200

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,6 +7,6 @@ set fish_greeting
 set -gx FZF_DEFAULT_COMMAND 'find . -type d \( -name .git -o -name node_modules -o -iname "*cache*" \) -prune -o -print'
 
 # Define common aliases
-alias cat 'bat --plain'
+alias cat 'bat --plain --pager=never'
 alias ls 'exa --oneline --icons --group-directories-first'
 alias clip 'xsel --clipboard'
