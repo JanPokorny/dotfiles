8f3c2be8af68c0f702090f844b8c43130a806889
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-04 11:06:04 +0200

    Create fundle_plugins.fish

---
+++ dot_config/fish/conf.d/fundle_plugins.fish
@@ -0,0 +1,12 @@
+fundle plugin edc/bass
+fundle plugin gazorby/fish-abbreviation-tips
+fundle plugin nickeb96/puffer-fish
+fundle plugin jorgebucaran/autopair.fish
+fundle plugin oh-my-fish/plugin-thefuck
+fundle plugin oh-my-fish/plugin-direnv
+fundle plugin decors/fish-colored-man
+fundle plugin kidonng/zoxide.fish
+fundle plugin lig/fish-gitmoji --url https://codeberg.org/lig/fish-gitmoji.git
+fundle plugin wfxr/forgit
+
+fundle init
