5837ebb1b86230186a63dee98c5c7b608e01a601
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-05 12:24:07 +0200

    Update zoxide_config.fish

--- dot_config/fish/conf.d/zoxide_config.fish
+++ dot_config/fish/conf.d/zoxide_config.fish
@@ -1,4 +1,3 @@
 status is-interactive || exit 0
-set -gx _ZO_FZF_OPTS "--preview 'echo {} | cut -f2 | xargs lsd --group-directories-first --color=always --icon=always' --preview-window=down --no-sort --keep-right --height=45% --layout=reverse --tabstop=1"
 zoxide init fish | source
 bind \ez 'zi; commandline -f repaint'
