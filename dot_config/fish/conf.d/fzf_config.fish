c233274739bae6b5e0fc9a361420c7baf9924003
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-05 13:04:59 +0200

    Update fzf_config.fish

--- dot_config/fish/conf.d/fzf_config.fish
+++ dot_config/fish/conf.d/fzf_config.fish
@@ -3,5 +3,7 @@ set -gx FZF_DEFAULT_COMMAND "find . -mindepth 1 -type d \( -name .git -o -name n
 set -gx FZF_CTRL_T_COMMAND  "find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname '*cache*' \) -prune -o -print"
 set -gx FZF_ALT_C_COMMAND   "find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname '*cache*' \) -prune -o -type d -print"
 
-set -gx FZF_CTRL_T_OPTS     "--preview 'echo {} | cut -f2 | xargs lsd --group-directories-first --color=always --icon=always' --preview-window=down --no-sort --keep-right --height=45% --layout=reverse --tabstop=1"
-set -gx _ZO_FZF_OPTS        "--preview 'echo {} | cut -f2 | xargs lsd --group-directories-first --color=always --icon=always' --preview-window=down --no-sort --keep-right --height=45% --layout=reverse --tabstop=1"
+set -l  FZF_PREVIEW_OPTS    "--preview 'echo {} | cut -f2 | begin; read x; [ -f $x ] && bat --color=always --plain $x || lsd --group-directories-first --color=always --icon=always $x; end;' --preview-window=down --no-sort --keep-right --height=45% --layout=reverse --tabstop=1"
+set -gx FZF_CTRL_T_OPTS     $FZF_PREVIEW_OPTS
+set -gx FZF_ALT_C_OPTS      $FZF_PREVIEW_OPTS
+set -gx _ZO_FZF_OPTS        $FZF_PREVIEW_OPTS
