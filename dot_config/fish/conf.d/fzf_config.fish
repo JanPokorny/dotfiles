3f40677cf39d6614d88d94e85bf41d6996ca8fd2
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-25 16:27:19 +0200

    Update fzf_config.fish

--- dot_config/fish/conf.d/fzf_config.fish
+++ dot_config/fish/conf.d/fzf_config.fish
@@ -6,7 +6,7 @@ set -gx FZF_DEFAULT_COMMAND "find . -mindepth 1 -type d \( -name .git -o -name n
 set -gx FZF_CTRL_T_COMMAND  "find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname '*cache*' \) -prune -o -print"
 set -gx FZF_ALT_C_COMMAND   "find . -mindepth 1 -type d \( -name .git -o -name node_modules -o -iname '*cache*' \) -prune -o -type d -print"
 
-set -l  FZF_PREVIEW_OPTS    "--preview 'preview (echo {} | cut -f2)' --preview-window=down --keep-right --height=45% --layout=reverse --tabstop=1"
+set -l  FZF_PREVIEW_OPTS    "--preview 'preview (echo {} | cut -f2)' --preview-window=down --keep-right --layout=reverse"
 set -gx FZF_CTRL_T_OPTS     "$FZF_PREVIEW_OPTS"
 set -gx FZF_ALT_C_OPTS      "$FZF_PREVIEW_OPTS"
-set -gx _ZO_FZF_OPTS        "$FZF_PREVIEW_OPTS --no-sort --with-nth=2"
+set -gx _ZO_FZF_OPTS        "$FZF_PREVIEW_OPTS --no-sort"
