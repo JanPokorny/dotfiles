771883e193429a451b0ba01c1d3fd3c5e0ff86db
Jan Pokorný <jenompokorny@gmail.com>
2023-05-29 14:25:35 +0200

    Add ranger keybinding

--- dot_config/fish/conf.d/90_keybindings.fish
+++ dot_config/fish/conf.d/90_keybindings.fish
@@ -1,3 +1,7 @@
 # Alt+Shift+Up => cd ..
 bind \e\e\[A 'cd ..; commandline -f repaint'
 bind \e\[1\;4A 'cd ..; commandline -f repaint'
+
+# Alt+R => ranger
+bind \er 'ranger; commandline -f repaint'
+
