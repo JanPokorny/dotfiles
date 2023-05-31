6b4d044febe23f728684986816d742f12208182c
Jan Pokorný <JenomPokorny@gmail.com>
2023-05-31 12:16:16 +0200

    Update run_once_after_10-install-fonts.sh

--- .chezmoiscripts/run_once_after_10-install-fonts.sh
+++ .chezmoiscripts/run_once_after_10-install-fonts.sh
@@ -6,7 +6,7 @@ echo "### INSTALL FONTS ###"
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
-wget -O https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFont-Medium.ttf
-wget -O https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFontMono-Medium.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFont-Medium.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFontMono-Medium.ttf
 
 fc-cache -v -f .
