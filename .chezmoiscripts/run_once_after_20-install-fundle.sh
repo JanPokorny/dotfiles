7f27fbb86893ee4f5c0703bd002eb818c8aaeb4d
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-05 13:47:18 +0200

    Update run_once_after_20-install-fundle.sh

--- .chezmoiscripts/run_once_after_20-install-fundle.sh
+++ .chezmoiscripts/run_once_after_20-install-fundle.sh
@@ -2,4 +2,4 @@
 
 echo
 echo "### INSTALL FUNDLE ###"
-curl -sL https://git.io/fundle-install | /home/linuxbrew/.linuxbrew/bin/fish
+curl -sfL https://git.io/fundle > ${XDG_CONFIG_HOME:-~/.config}/fish/functions/fundle.fish;
