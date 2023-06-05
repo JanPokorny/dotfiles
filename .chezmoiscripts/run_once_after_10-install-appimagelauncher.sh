7b15e46646c58400c5c77f7ed557e25b093cc763
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-05 14:16:37 +0200

    Update run_once_after_10-install-appimagelauncher.sh

--- .chezmoiscripts/run_once_after_10-install-appimagelauncher.sh
+++ .chezmoiscripts/run_once_after_10-install-appimagelauncher.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo
+echo "### INSTALL APPIMAGELAUNCHER ###"
+
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:appimagelauncher-team/stable
 sudo apt update
