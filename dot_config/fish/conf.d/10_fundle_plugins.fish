1c94d9ac4048828f9457f84879708f5e69341c74
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-06 16:33:16 +0200

    Update 10_fundle_plugins.fish

--- dot_config/fish/conf.d/10_fundle_plugins.fish
+++ dot_config/fish/conf.d/10_fundle_plugins.fish
@@ -3,7 +3,6 @@ status is-interactive || exit 0
 fundle plugin edc/bass
 fundle plugin gazorby/fish-abbreviation-tips
 fundle plugin jorgebucaran/autopair.fish
-fundle plugin oh-my-fish/plugin-thefuck
 fundle plugin oh-my-fish/plugin-direnv
 fundle plugin decors/fish-colored-man
 fundle plugin lig/fish-gitmoji --url https://codeberg.org/lig/fish-gitmoji.git
