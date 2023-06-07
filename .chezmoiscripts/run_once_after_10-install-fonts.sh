893875caf990773cbbcc9377e93593fe192f7551
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-07 13:46:57 +0200

    Update run_once_after_10-install-fonts.sh

--- .chezmoiscripts/run_once_after_10-install-fonts.sh
+++ .chezmoiscripts/run_once_after_10-install-fonts.sh
@@ -6,7 +6,10 @@ echo "### INSTALL FONTS ###"
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 
+# JetBrains Mono NF
 wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFont-Medium.ttf
 wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Medium/JetBrainsMonoNerdFontMono-Medium.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Bold/JetBrainsMonoNerdFont-Bold.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Bold/JetBrainsMonoNerdFontMono-Bold.ttf
 
 fc-cache -v -f .
