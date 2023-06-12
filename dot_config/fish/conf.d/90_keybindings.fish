3f5c5528c94f75c59aecf7f295980594d61faa2d
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-12 15:57:33 +0200

    Delete 90_keybindings.fish

--- dot_config/fish/conf.d/90_keybindings.fish
+++
@@ -1,7 +0,0 @@
-# Alt+Shift+Up => cd ..
-bind \e\e\[A 'cd ..; commandline -f repaint'
-bind \e\[1\;4A 'cd ..; commandline -f repaint'
-
-# Alt+R => ranger
-bind \er 'ranger; commandline -f repaint'
-
