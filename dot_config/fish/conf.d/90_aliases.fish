29482146508eb7c6efd4f7bb42436433caeac4bd
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-12 16:08:40 +0200

    Update 90_aliases.fish

--- dot_config/fish/conf.d/90_aliases.fish
+++ dot_config/fish/conf.d/90_aliases.fish
@@ -1,4 +1,8 @@
 status is-interactive || exit 0
 
+# Better versions of standard commands (+many more by plugin-grc)
 alias cat 'bat --plain --pager=never'
 alias ls 'lsd --group-directories-first'
+
+# Abbrs
+abbr r ranger
