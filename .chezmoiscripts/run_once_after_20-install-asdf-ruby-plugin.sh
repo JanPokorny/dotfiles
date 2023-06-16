86a0e826f6cdff6f3c6f038f406412519e2f931f
Jan Pokorný <JenomPokorny@gmail.com>
2023-06-16 18:55:01 +0200

    Update run_once_after_20-install-asdf-ruby-plugin.sh

--- .chezmoiscripts/run_once_after_20-install-asdf-ruby-plugin.sh
+++ .chezmoiscripts/run_once_after_20-install-asdf-ruby-plugin.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo
-echo "### INSTALL ASDF RUBY PLUGIN ###"
+echo "### INSTALL ASDF PLUGINS ###"
+
+echo "# Ruby"
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby
 /home/linuxbrew/.linuxbrew/bin/fish -c 'set -Ux RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@3)"'
+
+echo "# NodeJS"
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
