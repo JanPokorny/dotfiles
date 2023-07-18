5eb34f4eb7e17940792acbe19f08eae64958706a
Jan Pokorný <JenomPokorny@gmail.com>
2023-07-18 18:18:48 +0200

    Update and rename run_once_after_10-install-appimagelauncher.sh to run_once_after_10-install-apt-dependencies.sh

--- .chezmoiscripts/run_once_after_10-install-apt-dependencies.sh
+++ .chezmoiscripts/run_once_after_10-install-apt-dependencies.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo
-echo "### INSTALL APPIMAGELAUNCHER ###"
+echo "### INSTALL APT DEPENDENCIES ###"
 
+# AppImage Launcher
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:appimagelauncher-team/stable
 sudo apt update
 sudo apt install appimagelauncher
+
+# Simple X HotKey Deamon
+sudo apt install sxhkd
