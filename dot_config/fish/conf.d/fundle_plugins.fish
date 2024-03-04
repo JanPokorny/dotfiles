b1991131cf1005df3e43744b89f4e16862984105
Jan Pokorný <JenomPokorny@gmail.com>
2024-03-04 14:44:41 +0100

    Update and rename 10_fundle_plugins.fish to fundle_plugins.fish

--- dot_config/fish/conf.d/fundle_plugins.fish
+++ dot_config/fish/conf.d/fundle_plugins.fish
@@ -7,7 +7,6 @@ fundle plugin oh-my-fish/plugin-direnv
 fundle plugin decors/fish-colored-man
 fundle plugin lig/fish-gitmoji --url https://codeberg.org/lig/fish-gitmoji.git
 fundle plugin oh-my-fish/plugin-grc
-fundle plugin z11i/github-copilot-cli.fish
 fundle plugin rstacruz/fish-asdf
 
 fundle init
