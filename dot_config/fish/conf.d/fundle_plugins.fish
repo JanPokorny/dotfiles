a57b2bf484847c9735e187bf86a13f57967b99d2
Jan Pokorný <JenomPokorny@gmail.com>
2024-04-22 14:34:04 +0200

    Update fundle_plugins.fish

--- dot_config/fish/conf.d/fundle_plugins.fish
+++ dot_config/fish/conf.d/fundle_plugins.fish
@@ -5,7 +5,6 @@ fundle plugin gazorby/fish-abbreviation-tips
 fundle plugin jorgebucaran/autopair.fish
 fundle plugin oh-my-fish/plugin-direnv
 fundle plugin decors/fish-colored-man
-fundle plugin lig/fish-gitmoji --url https://codeberg.org/lig/fish-gitmoji.git
 fundle plugin oh-my-fish/plugin-grc
 fundle plugin rstacruz/fish-asdf
 
