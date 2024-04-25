57abca2631bdc77db1a74a0d9b50ee89291ebce3
Jan Pokorný <jenompokorny@gmail.com>
2024-04-25 12:52:07 +0200

    Cleanup conf.d

--- dot_config/fish/conf.d/80_aliases.fish
+++ dot_config/fish/conf.d/80_aliases.fish
@@ -2,7 +2,7 @@ status is-interactive || exit 0
 
 # Better versions of standard commands (+many more by plugin-grc)
 alias cat 'bat --plain --pager=never'
-alias ls 'lsd --group-directories-first'
+alias ls 'eza --icons --group-directories-first'
 
 # Abbrs
 abbr r ranger
