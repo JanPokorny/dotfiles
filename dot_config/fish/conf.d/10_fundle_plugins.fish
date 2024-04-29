c980b4d4400f93ddf8d616e148d80de67f774a57
Jan Pokorný <jenompokorny@gmail.com>
2024-04-29 13:39:39 +0200

    Switch asdf -> mise

--- dot_config/fish/conf.d/10_fundle_plugins.fish
+++ dot_config/fish/conf.d/10_fundle_plugins.fish
@@ -2,10 +2,7 @@ status is-interactive || exit 0
 
 fundle plugin edc/bass
 fundle plugin gazorby/fish-abbreviation-tips
-fundle plugin jorgebucaran/autopair.fish
-fundle plugin oh-my-fish/plugin-direnv
 fundle plugin decors/fish-colored-man
 fundle plugin oh-my-fish/plugin-grc
-fundle plugin rstacruz/fish-asdf
 
 fundle init
