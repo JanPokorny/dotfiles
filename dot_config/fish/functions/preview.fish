9a08e270585c108b3e31f2d9bcf64f4543750000
Jan Pokorný <JenomPokorny@gmail.com>
2025-01-07 10:23:11 +0100

    Switch viu -> chafa
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- dot_config/fish/functions/preview.fish
+++ dot_config/fish/functions/preview.fish
@@ -2,6 +2,6 @@ function preview -a path
   if [ -d $path ]
     lsd --group-directories-first --color=always --icon=always $path
   else
-    viu $path 2>/dev/null || bat --color=always --plain $path
+    chafa $path 2>/dev/null || bat --color=always --plain $path
   end
 end
