047da41f6c9e69688a559b0f4287a5f443c9ef26
Jan Pokorný <JenomPokorny@gmail.com>
2025-09-01 10:11:37 +0200

    Simplify install scripts
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- .chezmoiscripts/run_once_after_20-setup-fish.sh
+++ .chezmoiscripts/run_once_after_20-setup-fish.sh
@@ -2,12 +2,6 @@
 
 echo
 echo "### SETUP FISH ###"
-
-if [[ $(uname) == "Darwin" ]]; then
-  FISH_PATH="/opt/homebrew/bin/fish"
-else
-  FISH_PATH="/home/linuxbrew/.linuxbrew/bin/fish"
-fi
-
-echo $FISH_PATH | sudo tee -a /etc/shells
-chsh -s $FISH_PATH
+source ~/.profile
+which fish | sudo tee -a /etc/shells
+chsh -s $(which fish)
--- dot_config/fish/conf.d/20_zoxide.fish
+++ dot_config/fish/conf.d/20_zoxide.fish
@@ -1,3 +1,2 @@
 status is-interactive || exit 0
 zoxide init fish | source
-bind \ez 'zi; commandline -f repaint'
